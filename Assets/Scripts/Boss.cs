1f298783c3b4f29b4249a3f36947252e773dae3f
Stas <stanislav.ivanov.2004@internet.ru>
2023-05-28 17:00:26 +0500

    Add Boss

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -7,23 +7,30 @@ public class Boss : MonoBehaviour
 {
     public Transform target;
     public Rigidbody2D rb;
+    public Animator animator;
 
     //public float damage;
     public float longDistance;
     public float shortDistance;
     public float speed;
-    public float force;
 
-    //private static DateTime dateTime;
-    //private static TimeSpan delta;
+    public bool isFacingRight;
 
-    //public GameObject bullet;
-    //public Transform shotPoint;
+    private static DateTime dateTime;
+    private static TimeSpan deltaState;
+    private static TimeSpan deltaFire;
+
+    private bool isFire;
+
+    public Bullet bullet;
+    public Transform shotPoint;
 
     void Start()
     {
-        //dateTime = DateTime.Now;
-        //delta = new TimeSpan(0, 0, 0, 3, 0);
+        dateTime = DateTime.Now;
+        deltaState = new TimeSpan(0, 0, 0, 1, 0);
+        deltaFire = new TimeSpan(0, 0, 0, 1, 10);
+        isFacingRight = true; 
     }
 
     // Update is called once per frame
@@ -35,11 +42,43 @@ public class Boss : MonoBehaviour
         }
         if (Vector2.Distance(transform.position, target.position) < shortDistance)
         {
-            Debug.Log("ok");
             var directionImpulse = (target.position - transform.position).normalized;
-            Debug.Log($"{directionImpulse}");
             rb.AddForce(directionImpulse * 20000, ForceMode2D.Force);
         }
+        Flip();
 
+        if (!isFire) 
+        {
+            if (DateTime.Now - dateTime > deltaState)
+            {
+                animator.SetBool("Fire", true);
+                isFire = true;
+                dateTime = DateTime.Now;                
+            }
+        }
+        else
+        {
+            if (DateTime.Now - dateTime > deltaFire)
+            {
+                animator.SetBool("Fire", false);
+                isFire = false;
+                dateTime = DateTime.Now;
+                bullet.direction = (target.position - transform.position).normalized;
+                var currBullet = Instantiate(bullet, shotPoint.position, transform.rotation);
+            }
+        }
+        
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && (target.position.x - transform.position.x) < 0f || !isFacingRight && (target.position.x - transform.position.x) > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            Debug.Log("a");
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
