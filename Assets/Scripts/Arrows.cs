e61cbb3d23de98bdf070525ea3be35fe48f137a9
Greenbelg <greenbelg@internet.ru>
2023-06-02 04:05:15 +0500

    add cool switch

--- Assets/Scripts/Arrows.cs
+++ Assets/Scripts/Arrows.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class Arrows : MonoBehaviour
 {
-    [SerializeField] private GameObject arrows;
     [SerializeField] private Animator gate;
     [SerializeField] private GameObject goNext;
+    [SerializeField] private GameObject player;
+    private bool isEnd = false;
 
     void Update()
     {
         var bosses = GameObject.FindGameObjectsWithTag("Boss");
-        if(bosses.Length == 0)
+        if(bosses.Length == 0 && !isEnd)
         {
-            arrows.SetActive(true);
+            isEnd = true;
             gate.SetBool("isEnd", true);
             goNext.SetActive(true);
+            player.transform.Translate(gate.transform.position.x - player.transform.position.x, 
+                gate.transform.position.y - player.transform.position.y - 5,
+                gate.transform.position.y - player.transform.position.z);
         }
     }
 
